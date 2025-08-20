f8801e3d64a411af23c7912836715459095e00fb
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-08-20 12:22:15 -0300

    agregado el test

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,8 +1,21 @@
 # módulo para organizar funciones o clases utilizadas en nuestro proyecto
 # Crear tantos módulos como sea necesario para organizar el 
 #TP1 Act1
+
+#Nodo
 class Nodo:
     def __init__ (self, dato):
         self.dato = dato
         self.siguiente = None
         self.anterior = None
+        
+#Lista enlazada vacia
+class ListaDobleEnlazada:
+    def__init__(self):
+        self.cabeza = None
+        self.cola = None
+        self.tamanio = 0
+        
+
+        
+    
