80bb5c4df209955919612fa60fa7e7bea7feb2c6
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-08-28 20:52:27 -0300

    prueba

--- TrabajoPractico_1/proyecto_1/main.py
+++ TrabajoPractico_1/proyecto_1/main.py
@@ -1,18 +1,1 @@
-lista = ListaDobleEnlazada()
 
-print("¿Está vacía?:", lista.esta_vacia())  # True
-
-lista.agregar_al_inicio(10)
-lista.agregar_al_final(20)
-lista.agregar_al_final(30)
-
-print("Lista:", lista)  # 10 <-> 20 <-> 30
-
-lista.insertar(5, 0)   # al inicio
-lista.insertar(25, 3)  # en el medio
-lista.insertar(40)     # al final
-
-print("Lista después de insertar:", lista)
-# 5 <-> 10 <-> 20 <-> 25 <-> 30 <-> 40
-
-print("Tamaño:", len(lista))  # 6
