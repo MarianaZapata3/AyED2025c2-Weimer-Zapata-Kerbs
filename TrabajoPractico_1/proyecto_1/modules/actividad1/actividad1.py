f13fe1faa69bce996c0b071043cc358d5f2e025e
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-08-30 14:31:06 -0300

    Esta la actividad 1 lista

--- TrabajoPractico_1/proyecto_1/modules/actividad1/actividad1.py
+++ TrabajoPractico_1/proyecto_1/modules/actividad1/actividad1.py
@@ -71,11 +71,15 @@ class ListaDobleEnlazada:
         self.tamanio += 1                  # Incrementa el tamaño de la lista
 
     #Extraer
+    def extraer(self, posicion=None):    
         if self.esta_vacia():
             raise Exception("La lista está vacía")
 
         if posicion is None:
             posicion = self.tamanio - 1
+
+        if posicion < 0:
+            posicion += self.tamanio
 
         if posicion < 0 or posicion >= self.tamanio:
             raise Exception("Posición inválida")
@@ -121,19 +125,91 @@ class ListaDobleEnlazada:
 
     #Concatenar
     def concatenar(self, otra_lista):
-        if otra_lista.esta_vacia():
+        otra = otra_lista.copiar()  # <-- aseguramos que los punteros sean limpios
+        if otra.esta_vacia():
             return self
 
         if self.esta_vacia():
-            self.cabeza = otra_lista.cabeza
-            self.cola = otra_lista.cola
+            self.cabeza = otra.cabeza
+            self.cola = otra.cola
         else:
-            self.cola.siguiente = otra_lista.cabeza
-            otra_lista.cabeza.anterior = self.cola
-            self.cola = otra_lista.cola
+            self.cola.siguiente = otra.cabeza
+            otra.cabeza.anterior = self.cola
+            self.cola = otra.cola
 
-        self.tamanio += otra_lista.tamanio
+        self.cabeza.anterior = None
+        self.tamanio += otra.tamanio
         return self
+
+    
+    #__len__(): devuelve el número de ítems de la lista
+    def __len__(self):
+        return self.tamanio
+
+    #__add__(self, otra_lista): suma de dos listas
+    def __add__(self, otra_lista):
+        if not isinstance(otra_lista, ListaDobleEnlazada):
+            raise Exception("Solo se pueden sumar listas dobles enlazadas")
+
+        # Usamos copiar() para no modificar la lista original
+        nueva = self.copiar()
+        nueva.concatenar(otra_lista.copiar())  # Concatenamos una copia de la otra lista
+        return nueva
+
+    #__iter__(): permite recorrer la lista con un for
+    def __iter__(self):
+        actual = self.cabeza
+        while actual:
+            yield actual.dato
+            actual = actual.siguiente
+
+#grafico para actividad 1 (NO EJECUTAR HASTA TENER TODO)
+import time
+import matplotlib.pyplot as plt
+from modules.actividad1.actividad1 import ListaDobleEnlazada
+import random
+
+# Tamaños de prueba
+N_list = list(range(100, 10001, 500))
+# Guardamos los tiempos
+tiempos_len = []
+tiempos_copiar = []
+tiempos_invertir = []
+
+for N in N_list:
+    lde = ListaDobleEnlazada()
+    for i in range(N):
+        lde.agregar_al_final(random.randint(0, 1000))
+
+    # medir len
+    start = time.perf_counter()
+    _ = len(lde)
+    end = time.perf_counter()
+    tiempos_len.append(end - start)
+
+    # medir copiar
+    start = time.perf_counter()
+    _ = lde.copiar()
+    end = time.perf_counter()
+    tiempos_copiar.append(end - start)
+
+    # medir invertir
+    start = time.perf_counter()
+    lde.invertir()
+    end = time.perf_counter()
+    tiempos_invertir.append(end - start)
+
+# Graficar
+plt.figure(figsize=(10,6))
+plt.plot(N_list, tiempos_len, marker='o', label='len()')
+plt.plot(N_list, tiempos_copiar, marker='s', label='copiar()')
+plt.plot(N_list, tiempos_invertir, marker='^', label='invertir()')
+plt.xlabel('Número de elementos N')
+plt.ylabel('Tiempo de ejecución (segundos)')
+plt.title('Tiempo de ejecución vs tamaño de lista')
+plt.legend()
+plt.grid(True)
+plt.show()
 
 
         
