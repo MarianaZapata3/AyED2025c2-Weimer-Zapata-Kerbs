ebae319bb5ddb1b9a62aa6084205f2ffbdd944c7
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-09-10 12:35:08 -0300

    agregado

--- TrabajoPractico_1/proyecto_1/modules/actividad3/ordenamientoburbuja.py
+++ TrabajoPractico_1/proyecto_1/modules/actividad3/ordenamientoburbuja.py
@@ -1,27 +1,32 @@
-# orden de complejidad del ordenamiento por selección es O(n^2)
-# en todos los casos (mejor, peor y promedio) su complejidad es O(n^2)
-# ya que siempre recorre toda la lista buscando el mínimo o máximo
-
-# el ordenamiento por selección funciona buscando en cada pasada
-# el elemento más grande (o más pequeño) y colocándolo en su lugar correcto.
-# en cada iteración, la parte ordenada de la lista crece de a un elemento.
-
 from random import randint
 
-def ordenamiento_por_seleccion(lista):
-    for llenar_ranura in range(len(lista) - 1, 0, -1):
-        posicion_del_mayor = 0
-        for ubicacion in range(1, llenar_ranura + 1):
-            if lista[ubicacion] > lista[posicion_del_mayor]:
-                posicion_del_mayor = ubicacion
-        lista[llenar_ranura], lista[posicion_del_mayor] = lista[posicion_del_mayor], lista[llenar_ranura]
+def ordenamiento_burbuja(lista):
+    for num_pasadas in range(len(lista)-1, 0, -1):
+        for j in range(num_pasadas):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+    return lista
+
+def ordenamiento_burbuja_corto(lista):
+    intercambiado = True
+    num_pasadas = len(lista)-1
+    while num_pasadas > 0 and intercambiado:
+        intercambiado = False
+        for j in range(num_pasadas):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                intercambiado = True
+        num_pasadas -= 1
     return lista
 
 if __name__ == '__main__':
+    # Lista de prueba: mínimo 500 números de 5 dígitos
     N = 500
     datos = [randint(10000, 99999) for _ in range(N)]
-    datos_ordenados = ordenamiento_por_seleccion(datos.copy())
-    print("Primeros 20 ordenados (selección):", datos_ordenados[:20])
+    datos_ordenados = ordenamiento_burbuja(datos.copy())
+    print("Primeros 20 ordenados (burbuja):", datos_ordenados[:20])
+
+
 
 
 
