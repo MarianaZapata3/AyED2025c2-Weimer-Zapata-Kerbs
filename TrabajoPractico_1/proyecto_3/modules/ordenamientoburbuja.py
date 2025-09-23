c1080c3d7c8c2e0ebf57ec0d2600436086b97e1a
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-09-23 19:03:55 -0300

    Agregué los algoritmos de ordenamiento y pruebas

--- TrabajoPractico_1/proyecto_3/modules/ordenamientoburbuja.py
+++ TrabajoPractico_1/proyecto_3/modules/ordenamientoburbuja.py
@@ -1,25 +1,10 @@
 from random import randint
 
-# ---------------------------
-# Ordenamiento burbuja clásico
-# ---------------------------
-def ordenamiento_burbuja(lista):
-    # Bucle externo: controla las pasadas
-    for num_pasadas in range(len(lista)-1, 0, -1):
-        # Bucle interno: recorre hasta num_pasadas
-        for j in range(num_pasadas):
-            # Comparación entre elementos adyacentes
-            if lista[j] > lista[j+1]:
-                # Intercambio si están desordenados
-                lista[j], lista[j+1] = lista[j+1], lista[j]
-    return lista
-
-
 # ----------------------------------
 # Ordenamiento burbuja optimizado
 # (se corta si la lista ya está ordenada)
 # ----------------------------------
-def ordenamiento_burbuja_corto(lista):
+def ordenamiento_burbuja(lista):
     intercambiado = True                 # bandera para detectar cambios
     num_pasadas = len(lista)-1           # cantidad de pasadas posibles
     while num_pasadas > 0 and intercambiado:
@@ -41,13 +26,10 @@ if __name__ == '__main__':
     N = 500
     datos = [randint(10000, 99999) for _ in range(N)]
 
-    # Usamos burbuja clásico
+    # Usamos burbuja 
     datos_ordenados = ordenamiento_burbuja(datos.copy())
     print("Primeros 20 ordenados (burbuja clásico):", datos_ordenados[:20])
 
-    # Usamos burbuja optimizado
-    datos_ordenados_corto = ordenamiento_burbuja_corto(datos.copy())
-    print("Primeros 20 ordenados (burbuja corto):  ", datos_ordenados_corto[:20])
 
 
 
