ffe2c94926efaed106710cfb6139a257d54f5d9a
MarianaZapata3 <marianagabrielazapata@gmail.com>
2025-10-01 21:32:46 -0300

    agregado

--- TrabajoPractico_2/proyecto_1/modules/saladeemergencia.py
+++ TrabajoPractico_2/proyecto_1/modules/saladeemergencia.py
@@ -1,30 +1,28 @@
 # -*- coding: utf-8 -*-
+
 import time
 import datetime
 import random
 import modules.pacientes as pac
 from modules.coladeprioridad import ColaPrioridad
 
-n = 20  # cantidad total de pacientes que llegarán
+n = 20  # ciclos de simulación
 
 cola_de_espera = ColaPrioridad()
-pacientes_llegados = 0  # contador de pacientes que ya llegaron
 
-while pacientes_llegados < n or not cola_de_espera.esta_vacia():
+for i in range(n):
     ahora = datetime.datetime.now()
     fecha_y_hora = ahora.strftime('%d/%m/%Y %H:%M:%S')
     print('-*-'*15)
     print('\n', fecha_y_hora, '\n')
 
-    # Crear paciente y encolarlo según riesgo si aún no llegaron todos
-    if pacientes_llegados < n:
-        paciente = pac.Paciente()
-        pacientes_llegados += 1
-        print('Llega paciente:', paciente)
-        cola_de_espera.encolar(paciente, paciente.get_riesgo())
+    # Crear paciente y encolarlo según riesgo
+    paciente = pac.Paciente()
+    print('Llega paciente:', paciente)
+    cola_de_espera.encolar(paciente, paciente.get_riesgo())
 
     # Atender paciente en 50% de los casos
-    if not cola_de_espera.esta_vacia() and random.random() < 0.5:
+    if random.random() < 0.5 and not cola_de_espera.esta_vacia():
         paciente_atendido = cola_de_espera.desencolar()
         print('*'*40)
         print('Se atiende el paciente:', paciente_atendido)
@@ -37,8 +35,7 @@ while pacientes_llegados < n or not cola_de_espera.esta_vacia():
     print('-*-'*15)
     time.sleep(1)
 
-#Mensaje final
-print("No hay mas pacientes por atender") 
+
 
 
 
